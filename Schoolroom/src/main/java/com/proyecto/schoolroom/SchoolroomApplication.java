cf0d8c9f7f70592e976f21fe4c044f65d6e62c1c
frank <frank@DESKTOP-V872H03>
2020-03-03 09:58:46 -0600

    Creacion de Proyecto

---
+++ Schoolroom/src/main/java/com/proyecto/schoolroom/SchoolroomApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.schoolroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolroomApplication.class, args);
+	}
+
+}
